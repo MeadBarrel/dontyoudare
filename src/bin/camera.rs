fe63440a1933d97b7ad324c3189f0e860c7e7238
Natalia Borovskikh <lai.tash@gmail.com>
2022-11-26 10:14:53 -0300

    Move core functionality of camera.rs to a separate class

--- src/bin/camera.rs
+++ src/bin/camera.rs
@@ -16,33 +16,20 @@ use ropencv::signals::*;
 
 
 pub fn run(sender: Sender, receiver: Receiver) -> Result<()> {
-    let mut camera = prepare_camera()?;
-    let mut motiondetect = configure()?.create(sender);
-    let mut camera_running = true;
+    let camera = prepare_camera()?;
+    let motiondetect = configure()?.create(sender);
+
+    let mut runner = CameraRunner::new(camera, motiondetect);
 
     loop {
         match receiver.try_recv() {
-            Ok(Signal::StopCamera) => {
-                info!("Stopping Camera");
-                camera_running = false;
-            },
-            Ok(Signal::StartCamera) => {
-                info!("(Re)starting Camera");
-                camera_running = true
-            },
-            Ok(_) | Err(_) => {},
-        }
+            Ok(signal) => {
+                runner.handle_signal(signal)?;
+            }
+            Err(_) => { }
+        };
 
-        if !camera_running {
-            continue
-        }
-
-        let mut frame = read_camera(&mut camera)?;
-
-
-        motiondetect = motiondetect.new_frame(&frame)?;
-
-        imshow("Camera", &frame);
+        runner = runner.next()?;
 
         let key = wait_key(1)?;
         if key == 113 {
@@ -52,6 +39,61 @@ pub fn run(sender: Sender, receiver: Receiver) -> Result<()> {
     }
 
     Ok(())
+}
+
+
+pub struct  CameraRunner {
+    camera: VideoCapture,
+    motiondetect: MotionDetect,
+
+    camera_running: bool,
+}
+
+
+impl CameraRunner {
+    pub fn new(camera: VideoCapture, motiondetect: MotionDetect) -> Self {
+        Self {
+            camera,
+            motiondetect,
+            camera_running: true
+        }
+    }
+
+    pub fn next(mut self) -> Result<Self> {
+        if !self.camera_running { return Ok(self) }
+
+        let mut frame = self.read_camera()?;
+        self.motiondetect = self.motiondetect.new_frame(&frame)?;
+
+        self.show_frame(&frame)?;
+
+        Ok(self)
+    }
+
+    fn show_frame(&self, frame: &Mat) -> Result<()> {
+        Ok(imshow("Camera", frame)?)
+    }
+
+    fn handle_signal(&mut self, signal: Signal) -> Result<()> {
+        info!("HANDLING SIGNAL");
+        match signal {
+            Signal::StopCamera => {
+                info!("Stopping Camera");
+                self.camera_running = false;
+            },
+            Signal::StartCamera => {
+                info!("(Re)starting Camera");
+                self.camera_running = true;
+            },
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn read_camera(&mut self) -> Result<Mat> {
+        read_camera(&mut self.camera)
+    }
+
 }
 
 
